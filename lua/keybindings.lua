b64c13c1660b351326a18a6843806dd5f3342340
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-12-17 20:48:50 -0600

    installed snacks plugin

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -6,22 +6,3 @@ vim.keymap.set("n", "<leader>h", vim.cmd.split)
 
 -- Exit insert mode
 vim.keymap.set("i", "jk", "<ESC>")
-
--- Save and exit files
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set("n", "<leader>q", ":wq!<CR>")
-
--- Clear buffer
-vim.keymap.set("n", "<leader>bd", ":bd<CR>")
-
--- Better tabbing
-vim.keymap.set("v", "<TAB>", ">gv")
-vim.keymap.set("v", "<S-TAB>", "<gv")
-
--- Move selected line up and down
-vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv")
-vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv")
-
--- CLose buffer
-vim.keymap.set("n", "<leader>cb", ":bd<CR>")
-vim.keymap.set("n", "<leader>db", ":bw<CR>")
--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,25 +1,7 @@
 return {
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-        },
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup()
-            vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>")
-        end,
-    },
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup()
+        vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>")
+    end,
 }
