e9d0560b205f1d631a9bcbd1a11ccb2d064ba8c3
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-12-17 20:50:32 -0600

    enabled indents

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,7 +9,7 @@ return {
         -- refer to the configuration section below
         bigfile = { enabled = true },
         dashboard = { enabled = true },
-        indent = { enabled = false },
+        indent = { enabled = true },
         input = { enabled = true },
         notifier = { enabled = true },
         quickfile = { enabled = true },
