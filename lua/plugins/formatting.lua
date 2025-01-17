9dafbdcfaf723b07510357a6c7ecfc72229857ca
Ezequiel Buck <ezequiel.bm04@gmail.com>
2025-01-16 18:24:12 -0600

    changed python formatter black -> autopep8

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -18,7 +18,7 @@ return {
 		-- Define your formatters
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = { "isort", "black" },
+			python = { "isort", "autopep8" },
 			c = { "clang-format" },
 			cpp = { "clang-format" },
 			bash = { "shfmt" },
