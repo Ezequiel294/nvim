d83556eb22f9520bc858d2c0d38ce85be687c234
Ezequiel Buck <ezequiel.bm04@gmail.com>
2025-02-11 13:46:44 -0600

    minor changed

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -3,6 +3,3 @@ vim.g.mapleader = " "
 -- Vertical and horizontal split
 vim.keymap.set("n", "<leader>v", vim.cmd.vsp)
 vim.keymap.set("n", "<leader>h", vim.cmd.split)
-
--- CLose buffer
-vim.keymap.set("n", "<leader>cb", ":bd<CR>")
