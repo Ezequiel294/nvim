9bae8e2f76542512d15a499dab63ff66ad1aaa34
Ezequiel Buck <ezequielbuckmartinez@proton.me>
2025-12-20 22:59:09 -0600

    solved issue with treesitter moving from master to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter").setup({
 				sync_install = false,
 				auto_install = true,
 				highlight = {
